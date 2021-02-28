439476edeb570b0e01284f139201d5d916de29b5
kehinde <kehindesalaudeen222@gmail.com>
2021-02-28 14:10:34 +0100

    redirects session

--- src/components/Extras/session.js
+++ src/components/Extras/session.js
@@ -6,11 +6,14 @@ export const
                 window.location.assign("/admin")
             }
         } else if (!session) {
-            if (window.location.pathname !== "admin") {
+            if (window.location.pathname === "/admin") {
                 window.location.assign('/login')
             } 
-        } else {
+        } else if (session === null){
             localStorage.setItem("log", false)
+            if (window.location.pathname === "/admin") {
+                window.location.assign('/login')
+            } 
         }
     },
     log = (bool) => {
